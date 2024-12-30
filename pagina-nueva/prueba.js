bdcbf1dca1e0443efccf3d0faddcafe8b5d659e1
Dianaailyn <diana@communitysafeguardnetwork.org>
2024-12-30 13:17:06 -0600

    main

---
+++ pagina-nueva/prueba.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+
+hamburgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
+/**************************PÁGINA PRINCIPAL***************************** */
+/*CARRUSEL */
+
